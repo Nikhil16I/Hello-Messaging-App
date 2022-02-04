d6bb340b31539e577c6b57e4818e76aad2c3826b
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-02-04 20:28:44 +0530

    Creating a Rest Controller to show Hello message

---
+++ src/test/java/com/restapi/hellomessaging_app/HelloMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.hellomessaging_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
